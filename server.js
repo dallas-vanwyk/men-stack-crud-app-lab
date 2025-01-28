09a1fb6b0ec7637a86a4dd38d3c91c253feb01f9
dallas-vanwyk <dallas.vanwyk@gmail.com>
2025-01-27 18:27:54 -0600

    added most of the boilerplate

---
+++ server.js
@@ -0,0 +1,46 @@
+// server.js
+
+// ------------------------------------------------------- Dependencies
+
+// import and load express
+const express = require('express');
+const app = express();
+
+// add dotenv, load environment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+// add Mongoose
+const mongoose = require('mongoose');
+
+// define port variable
+const port = 3000;
+
+
+
+// ------------------------------------------------------- Middleware
+
+// connect to database
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+})
+
+
+// ------------------------------------------------------- Routes
+
+// GET / test route
+app.get("/", async (req, res) => {
+    res.render('index.ejs');
+});
+
+
+
+
+// ------------------------------------------------------- Port
+
+app.listen(port, () => {
+    console.log(`Listening on port`, port);
+});
+
